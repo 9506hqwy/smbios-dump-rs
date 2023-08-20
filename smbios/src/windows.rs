15324c5f1e190ddc77494bfd6b1cab990c2aec62
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-08-20 18:25:43 +0900

    依存ライブラリのバージョンを更新した。

--- smbios/src/windows.rs
+++ smbios/src/windows.rs
@@ -2,7 +2,7 @@ use super::RawSmbiosData;
 use bytes::Bytes;
 use windows::core::Error;
 use windows::Win32::System::SystemInformation::{
-    EnumSystemFirmwareTables, GetSystemFirmwareTable, FIRMWARE_TABLE_ID, FIRMWARE_TABLE_PROVIDER,
+    EnumSystemFirmwareTables, GetSystemFirmwareTable, FIRMWARE_TABLE_PROVIDER,
 };
 
 pub const FIRMWARE_TABLE_ACPI: u32 = 0x41435049; // 'ACPI'
@@ -23,41 +23,37 @@ fn enum_system_firmware_table(signature: u32) -> Result<Vec<u32>, Error> {
 
     let sig = FIRMWARE_TABLE_PROVIDER(signature);
 
-    let size = unsafe { EnumSystemFirmwareTables(sig, None, 0) };
-    if size == 0 {
-        return Err(Error::from_win32());
-    }
-
-    let mut buffer = vec![0u32; (size / 4) as usize];
-
-    let size = unsafe {
-        let buf = buffer.as_mut_ptr() as *mut FIRMWARE_TABLE_ID;
-        EnumSystemFirmwareTables(sig, Some(buf), size)
-    };
-    if size == 0 {
-        return Err(Error::from_win32());
-    }
-
-    Ok(buffer)
-}
-
-fn get_system_firmware_table(signature: u32, table_id: u32) -> Result<Vec<u8>, Error> {
-    // https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemfirmwaretable
-
-    let sig = FIRMWARE_TABLE_PROVIDER(signature);
-    let id = FIRMWARE_TABLE_ID(table_id);
-
-    let size = unsafe { GetSystemFirmwareTable(sig, id, None, 0) };
+    let size = unsafe { EnumSystemFirmwareTables(sig, None) };
     if size == 0 {
         return Err(Error::from_win32());
     }
 
     let mut buffer = vec![0u8; size as usize];
 
-    let size = unsafe {
-        let buf = buffer.as_mut_ptr() as *mut std::ffi::c_void;
-        GetSystemFirmwareTable(sig, id, Some(buf), size)
-    };
+    let size = unsafe { EnumSystemFirmwareTables(sig, Some(buffer.as_mut_slice())) };
+    if size == 0 {
+        return Err(Error::from_win32());
+    }
+
+    Ok(buffer
+        .chunks_exact(4)
+        .map(|b| u32::from_le_bytes(b.try_into().unwrap()))
+        .collect())
+}
+
+fn get_system_firmware_table(signature: u32, table_id: u32) -> Result<Vec<u8>, Error> {
+    // https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemfirmwaretable
+
+    let sig = FIRMWARE_TABLE_PROVIDER(signature);
+
+    let size = unsafe { GetSystemFirmwareTable(sig, table_id, None) };
+    if size == 0 {
+        return Err(Error::from_win32());
+    }
+
+    let mut buffer = vec![0u8; size as usize];
+
+    let size = unsafe { GetSystemFirmwareTable(sig, table_id, Some(buffer.as_mut_slice())) };
     if size == 0 {
         return Err(Error::from_win32());
     }
